2329b9e9668113daa1cd974657126ab7f2bf9aa0
cookieukw <cookieukw@users.noreply.github.com>
2026-06-10 23:33:53 -0300

    chore: update debug bounding box description to be more inclusive of all UI elements

--- components/editor/start/customize-tab.tsx
+++ components/editor/start/customize-tab.tsx
@@ -186,7 +186,7 @@ export function CustomizeTab() {
                       <SlidersHorizontal className="w-3.5 h-3.5" />
                       Debug Bounding Boxes
                     </Label>
-                    <p className="text-xs text-muted-foreground">Draw outlines around all UI Groups.</p>
+                    <p className="text-xs text-muted-foreground">Draw outlines around all UI elements.</p>
                   </div>
                   <Switch
                     id="switch-bounding"
